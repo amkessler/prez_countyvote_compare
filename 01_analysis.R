f8511fc374dc097054f774b801105f85ecc3a26c
Aaron Kessler <kessler.aaron@gmail.com>
2019-05-21 01:09:17 -0400

    add pct point margin of victory

--- 01_analysis.R
+++ 01_analysis.R
@@ -37,7 +37,7 @@ prezcounties %>%
 
 #winnow down to just the needed columns
 prezcounties <- prezcounties %>% 
-  select(year, state_po, county, fips, party, candidatevotes)
+  select(year, state_po, county, fips, party, candidatevotes, totalvotes)
 
 #reshape the data to wide - put R and D results on same row ####
 prezcounties_wide <- prezcounties %>% 
@@ -46,6 +46,15 @@ prezcounties_wide <- prezcounties %>%
 #create column that calculates a party winner 
 prezcounties_wide <- prezcounties_wide %>% 
   mutate(winner = if_else(democrat > republican, "D", "R"))
+
+#calculate pct of vote and then pct point margin of victory
+prezcounties_wide <- prezcounties_wide %>% 
+  mutate(
+    pct_d = (democrat/totalvotes)*100,
+    pct_r = (republican/totalvotes)*100,
+    margin = round_half_up(abs(pct_d - pct_r),2)
+  )
+  
 
 #reshape again, this time to get all three cycles going across ####
 #with the party winners listed
@@ -58,11 +67,25 @@ three_cycles_across <- prezcounties_wide %>%
 #let's see what this looks like
 three_cycles_across
 
+#now we'll do something similar but this time for the margin victories for each cycle
+margins_across <- prezcounties_wide %>% 
+  select(year, county, state_po, fips, margin) %>%
+  spread(year, margin) %>% 
+  clean_names() %>% #to take care of column names that are all numbers (adds a preceding 'x')
+  arrange(state_po, county) %>%  #sort by state, then within each state by county 
+  select(fips, x2008_margin = x2008, x2012_margin = x2012, x2016_margin = x2016)
+
+margins_across
+
+#join to two tables together
+final <- inner_join(three_cycles_across, margins_across)
+
+
 #now that we have these results formatted, it's just a matter of
 #filtering based on the winner of each election
 
 #find counties with two obama wins and then a trump win
-three_cycles_across %>% 
+final %>% 
   filter(
     x2008 == "D", #Obama wins
     x2012 == "D", #Obama wins
@@ -70,7 +93,7 @@ three_cycles_across %>%
   )
 
 #save the result with a new name
-final_twoobama_trump <- three_cycles_across %>% 
+final_twoobama_trump <- final %>% 
   filter(
     x2008 == "D", #Obama wins
     x2012 == "D", #Obama wins
@@ -81,11 +104,11 @@ final_twoobama_trump <- three_cycles_across %>%
 write_csv(final_twoobama_trump, "final_twoobama_trump.csv")
 
 #now we'll do a similar one, but this time if Obama won EITHER in 2008 or 2012
-final_oneobama_trump <- three_cycles_across %>% 
+final_oneobama_trump <- final %>% 
   filter(
     (x2008 == "D" | x2012 == "D"), #Obama in either 2008 or 2012 - the "|" means "or"
     x2016 == "R"  #Trump wins
-  )
+    )
 
 #export to a file
 write_csv(final_oneobama_trump, "final_oneobama_trump.csv")
